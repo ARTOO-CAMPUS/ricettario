b82cf3059dd6d4e348b3a7eb8320fd21afa02b22
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-03-10 14:50:28 +0100

    route regex

--- server/risorse/ricette/index.js
+++ server/risorse/ricette/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var Ricette = require('./ricette.controller.js');
-
 //ROTTA CHE MI RESTITUISCE LE RICETTE DEL MONDO
 router.get('/', Ricette.getRicette);
+//ROTTA CHE MI RESTITUISCE UNA RICETTA
+router.get('/:id([0-9a-f]{24})', Ricette.dettaglioRicetta);
+//ROTTA PER CREARE LA RICETTA
+router.post('/', Ricette.creaRicetta);
 
 module.exports = router;
--- server/risorse/ricette/ricette.controller.js
+++ server/risorse/ricette/ricette.controller.js
@@ -3,8 +3,17 @@ module.exports = (function () {
 	var getRicette = function (req, res) {
 		res.send("le ricette del mondo");
 	}
+	var dettaglioRicetta = function (req, res) {
+		var id = req.params.id;
+		res.send("dettaglio ricetta con id " + id);
+	}
+	var creaRicetta = function (req, res) {
+		res.send("creo la ricetta");
+	}
 
 	return {
-		getRicette: getRicette
+		getRicette: getRicette,
+		dettaglioRicetta: dettaglioRicetta,
+		creaRicetta: creaRicetta,
 	}
 })();
