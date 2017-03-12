64dff93f440382b050aeae8010b6b5a146961bb8
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-03-12 22:17:33 +0100

    morgan

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const PORT = 3000;
 
 //IMPORTO IL FILE DI CONFIGURAZIONE PER IL DB
 require("./config/database.js");
+//IMPORTO IL FILE DI CONFIGURAZIONE PER IL LOGGER
+require("./config/logger.js")(app);
 
 require("./routes/routes.js")(app, express);
 //START DEL SERVER
