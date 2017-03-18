ae110cbf0a1c8d2d401d0f79d81e23eb602a5b66
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-03-18 12:47:38 +0100

    my-theme

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,13 @@
 angular.module('app', [
-	'ui.router',
-	'ngMaterial'
-]).run(function () {
-	console.log("App is run");
-});
+		'ui.router',
+		'ngMaterial'
+	]).run(function () {
+		console.log("App is run");
+	})
+	.config(function ($mdThemingProvider) {
+		$mdThemingProvider.theme('my-theme')
+			.primaryPalette('indigo')
+			.accentPalette('amber')
+			.warnPalette('red')
+			.backgroundPalette('blue-grey');
+	});
