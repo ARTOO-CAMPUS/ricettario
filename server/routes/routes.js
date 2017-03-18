74bc3feb25edd2600f30d9edfb7366bef874426a
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-03-18 12:24:24 +0100

    livereload e rotte

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,8 +2,21 @@ var path = require('path');
 var bodyParser = require('body-parser');
 module.exports = function (app, express) {
 
+	// LIVERELOAD
+	var livereload = require('livereload');
+	var server = livereload.createServer();
+	server.watch(path.join(__dirname, "..", "..", "client"));
+
 	//TRADUZIONE DEL BODY
 	app.use(bodyParser.json());
+
+	app.get('/', function (req, res) {
+		res.sendFile(path.join(__dirname, "..", "..", "client", "index.html"));
+	});
+
+	app.use("/", express.static(path.join(__dirname, "..", "..", "client")));
+	app.use("js", express.static(path.join(__dirname, "..", "..", "client", "js")));
+	app.use("css", express.static(path.join(__dirname, "..", "..", "client", "css")));
 
 	//ROTTA PER GLI UTENTI
 	app.use('/utenti', require("./../risorse/utenti"));
