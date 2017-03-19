e24e81ebb7fa147fba2dee895c8f48d797e15b9e
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-03-19 20:29:29 +0100

    sidenav with bug

--- client/app/ricette/home/home.controller.js
+++ client/app/ricette/home/home.controller.js
@@ -1,18 +1,11 @@
 angular.module('app').controller('HomeCtrl', function ($scope, $timeout, $mdSidenav, $log) {
 	$scope.close = function () {
-		// Component lookup should always be available since we are not using `ng-if`
 		$mdSidenav('left').close()
-			.then(function () {
-
-			});
+			.then(function () {});
 	};
 	$scope.open = function () {
-		console.log("toggle");
-		// Component lookup should always be available since we are not using `ng-if`
 		$mdSidenav('left').toggle()
-			.then(function () {
-
-			});
+			.then(function () {});
 	};
 
 
