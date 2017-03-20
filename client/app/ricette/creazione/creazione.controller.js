0e5d61dec9079a7d28a5d878dec9ed1b94decc34
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-03-20 15:28:34 +0100

    creazione ricetta

--- client/app/ricette/creazione/creazione.controller.js
+++ client/app/ricette/creazione/creazione.controller.js
@@ -1,17 +1,29 @@
-angular.module('app').controller('CreazioneCtrl', function ($scope, $rootScope, RicetteSrv) {
+angular.module('app').controller('CreazioneCtrl', function ($scope, $rootScope, RicetteSrv, $state) {
 
-	$scope.nuovaRicetta = {};
+	$scope.nuovaRicetta = {
+
+		titolo: '',
+		categoria: '',
+		immagini: [],
+		ingredienti: [],
+		difficolta: '',
+		tempodicottura: 0,
+		temperaturadicottura: 0,
+		preparazione: ''
+	};
 
 	$scope.creazione = function () {
 		$scope.nuovaRicetta.author = $rootScope.utente._id;
+		$scope.nuovaRicetta.immagini.push($scope.immagine);
 		RicetteSrv.createRicetta($scope.nuovaRicetta)
 			.then(function (data) {
-				console.log(data);
+				$state.go('dettaglio', {
+					id: data._id
+				});
 			})
 			.catch(function (err) {
 				console.log(err);
 			})
-
 	}
 
 })
